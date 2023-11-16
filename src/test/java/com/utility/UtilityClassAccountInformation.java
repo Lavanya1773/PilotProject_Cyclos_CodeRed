a5a16f4a0c0f9de2be4a9782c454fd2009290e73
Lavanya1773 <lavanyasethuraj1703@gmail.com>
2023-11-16 19:36:37 +0530

    Account Information feature files were uploaded by Lavanya

---
+++ src/test/java/com/utility/UtilityClassAccountInformation.java
@@ -0,0 +1,37 @@
+package com.utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class UtilityClassAccountInformation {
+	public String url, userName, password;
+	
+	public UtilityClassAccountInformation(){
+		data();
+	}
+	
+	public void data() {
+		File file = new File("src/test/resources/Cyclos.properties");
+		FileInputStream fio =null;
+		try {
+			fio = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Properties prop = new Properties();
+		try {
+			prop.load(fio);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		url = prop.getProperty("url");
+		userName = prop.getProperty("userName");
+		password = prop.getProperty("password");
+	}
+
+}
