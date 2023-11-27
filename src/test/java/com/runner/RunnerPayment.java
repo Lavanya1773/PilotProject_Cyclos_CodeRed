b7f50dd589c7cbb21f1938247926581c663f2713
Lavanya1773 <74286126+Lavanya1773@users.noreply.github.com>
2023-11-27 12:12:57 +0530

    RunnerPayment.java

--- src/test/java/com/runner/RunnerPayment.java
+++ src/test/java/com/runner/RunnerPayment.java
@@ -1,12 +1,1 @@
-//package com.runner;
-//import org.junit.runner.RunWith;
-//
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/resources/Features/Payments.feature", glue = "com.stepdefinitions",plugin = {"pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-//
-//public class RunnerPayment {
-//
-//}
+
