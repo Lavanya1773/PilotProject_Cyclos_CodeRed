f601dac58c2c83b7a9bf7284b256735f793c14ca
Lavanya1773 <74286126+Lavanya1773@users.noreply.github.com>
2023-11-27 12:18:42 +0530

    TestCodeRed_RunnerClass.java

--- src/test/java/com/runner/TestCodeRed_RunnerClass.java
+++ src/test/java/com/runner/TestCodeRed_RunnerClass.java
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
-//package com.runner;
-//
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-//
-//@CucumberOptions(features="C:\\Users\\lsethuraj\\eclipse-workspace\\CodeRed\\PilotProject_Cyclos_CodeRed\\src\\test\\resources\\Features",
-//glue="com.stepdefinitions",plugin = "json:target/cucumber-report.json")
-//
-//public class TestCodeRed_RunnerClass extends AbstractTestNGCucumberTests{
-// 
-//}
-=======
 package com.runner;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-<<<<<<< HEAD
-
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(features="C:\\Users\\alakshmanan\\eclipse-workspace\\CodeRed\\PilotProject_Cyclos_CodeRed\\src\\test\\resources\\Features",
+@CucumberOptions(features="src/test/resources/Features",
 glue="com.stepdefinitions",plugin = "json:target/cucumber-report.json")
 public class TestCodeRed_RunnerClass extends AbstractTestNGCucumberTests{
  
 }
-=======
-import io.cucumber.testng.CucumberOptions;
- 
-@CucumberOptions(features="C:\\Users\\lsethuraj\\eclipse-workspace\\CodeRed\\PilotProject_Cyclos_CodeRed\\src\\test\\resources\\Features",
-glue="com.stepdefinitions",plugin = "json:target/cucumber-report.json")
-public class TestCodeRed_RunnerClass extends AbstractTestNGCucumberTests{
-
-}
->>>>>>> e1b89dfce1145d8aaf16c43e166302f9b6bcfbe1
->>>>>>> ee91d922dd66522ba8552e58e39863a384d96d5c
