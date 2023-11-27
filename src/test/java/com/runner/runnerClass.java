9bce52064b664d6d5a4f0b5b46db7d5ce9086053
Lavanya1773 <74286126+Lavanya1773@users.noreply.github.com>
2023-11-27 12:12:40 +0530

    runnerClass.java

--- src/test/java/com/runner/runnerClass.java
+++ src/test/java/com/runner/runnerClass.java
@@ -1,14 +1,10 @@
-//
-//package com.runner;
-//import org.junit.runner.RunWith;
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//
-//@RunWith(Cucumber.class)
-////@CucumberOptions(tags="@MemberPayments", features = "src/test/resources/Features/AccountInformation.feature", glue = "com.stepdefinitions",plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-//@CucumberOptions(features = "src/test/resources/Features/Payments.feature", glue = "com.stepdefinitions",plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-////@CucumberOptions(features = "src/test/resources/Features", glue = "com.stepdefinitions",plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-//
-//public class runnerClass {
-//
-//}
+// package com.runner;
+
+// import org.junit.runner.RunWith;
+// import io.cucumber.junit.Cucumber;
+// import io.cucumber.junit.CucumberOptions;
+
+// @RunWith(Cucumber.class)
+// @CucumberOptions(features = "src/test/resources/Features", glue = "com.stepdefinitions",plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
+// public class runnerClass {
+// }
