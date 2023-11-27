eb8c88993a6e0f753c411a64001a63a8f4bdabed
Lavanya1773 <74286126+Lavanya1773@users.noreply.github.com>
2023-11-27 12:52:33 +0530

    GiftVoucherActions.java

--- src/test/java/com/actions/GiftVoucherActions.java
+++ src/test/java/com/actions/GiftVoucherActions.java
@@ -76,6 +76,8 @@ public class GiftVoucherActions {
 	public void printPDFclick() {
 		giftVoucherLocators.PrintPDF.click();
 	}
-	
+	public void clickLogout() {
+		giftVoucherLocators.logout.click();
+	}
 }
 
