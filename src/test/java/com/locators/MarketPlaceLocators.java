a0443adf209633eb55fce55d1d81987d119c46be
nirupama2011 <146086134+nirupama2011@users.noreply.github.com>
2023-11-27 12:14:32 +0530

    MarketPlaceLocators.java

--- src/test/java/com/locators/MarketPlaceLocators.java
+++ src/test/java/com/locators/MarketPlaceLocators.java
@@ -43,7 +43,7 @@ public class MarketPlaceLocators {
 	@FindBy(xpath="//a[@title='Search advertisements']")
 	public WebElement back; //xpath for going back
 	
-//	//back
+	//back
 	@FindBy(xpath="//span[text()='Price Lowest']")
 	public WebElement lowest; //xpath for lowest filter
 	
@@ -52,7 +52,7 @@ public class MarketPlaceLocators {
 
 	@FindBy(xpath="//*[name()=\"svg\" and @class=\"bi bi-heart\"]")
 	public WebElement fav2; //xpath for add to favorites
-//	//back	
+	//back	
 
 	@FindBy(xpath="//span[text()='Relevance']")
 	public WebElement relevance; //xpath for relevance filter
@@ -74,102 +74,6 @@ public class MarketPlaceLocators {
 	
 	@FindBy(xpath="//div[contains(text(),' result')]")
 	public WebElement resultCount; //xpath for getting the total counts
-	
-<<<<<<< HEAD
-
-}
-	
-//	@FindBy(xpath = "//*[name()=\"svg\" and @class=\"ci ci-login\"]") //xpath for signin tab 
-//	public WebElement signIn;
-//	
-//	@FindBy(xpath = "//input[@name='principal']") //xpath for username text box
-//	public WebElement userName;
-//	
-//	@FindBy(xpath = "//input[@name='password']") //xpath for password trxt box
-//	public WebElement password;
-//	
-//	@FindBy(xpath = "//button[@class='actionButton']") //xpath for sign button
-//	public WebElement signInButton;
-//	//MarketPlace locators.
-//	@FindBy(xpath="//div[@class='pageSectionHeading']") //xpath for the text that displayed after login
-//	public WebElement loginText;
-//	
-//	@FindBy(xpath="//*[name()=\"svg\" and @class=\"ci ci-shop-fill\"]") //xpath for marketplace tab
-//	public WebElement market;	
-//	
-//	@FindBy(xpath="(//button[@class='actionButton'])[2]") //xpath for list ads button
-//	public WebElement list;
-//	
-//	@FindBy(xpath="(//*[name()=\"svg\" and @class=\"bi bi-caret-down-fill\"])[3]") //xpath for clicking dropdown
-//	public WebElement drops;
-//	
-//	@FindBy(xpath="//span[text()='Price Highest']") //xpath for choosing Price highest filter
-//	public WebElement highest;
-//	
-//	@FindBy(xpath="//div[text()='test software']") //xpath for choosing product 1
-//	public WebElement product1;
-//	
-//	@FindBy(xpath="//span[text()='Add to favorites']") //xpath for adding to favorites
-//	public WebElement fav1;
-//	
-//	@FindBy(xpath="//div[text()='Added to favorites']") //xpath for popup text that displayed after adding product to favorites
-//	public WebElement popUp;
-//	
-//	@FindBy(xpath="//a[@title='Search advertisements']") //xpath for going back to search advertisements
-//	public WebElement back;
-//	
-//	@FindBy(xpath="//a[@class='breadCrumbLink']")
-//	public WebElement back2;
-//
-//	@FindBy(xpath="//div[text()='black brand burger']") //xpath for choosing product 2
-//	public WebElement product2;
-//	
-//	@FindBy(xpath="(//a[@class='itemsPanelLink linkPanel'])[1]") //xpath for adding to favorites
-//	public WebElement fav2;
-////	//back
-//	@FindBy(xpath="//span[text()='Price Lowest']") //xpath for choosing Price lowest filter
-//	public WebElement lowest;
-//	
-//	@FindBy(xpath="//div[text()='Ndulla']") //xpath for choosing product 3
-//	public WebElement product3;
-//
-//	@FindBy(xpath="//*[name()=\"svg\" and @class=\"bi bi-heart\"]") //xpath for adding to favorites
-//	public WebElement fav3;
-////	//back	
-//	
-////	@FindBy(xpath="(//td[@class='tiledResultThumbnail tiledResult-selectable'])[10]") //xpath for choosing product 4
-////	public WebElement product4;
-////	
-////	@FindBy(xpath="//span[text()='Add to favorites']") //xpath for adding to favorites
-////	public WebElement fav4;
-//	
-//	@FindBy(xpath="(//*[name()=\"svg\" and @class=\"ci ci-checkbox-unchecked\"])[1]") //xpath for enabling show only favorites checkbox
-//	public WebElement favOnly;
-//	
-////	@FindBy(xpath="//span[text()='Relevance']")
-////	public WebElement relevance;
-//	
-//	@FindBy(xpath="(//*[name()=\"svg\" and @class=\"bi bi-heart-fill\"])[1]")
-//	public WebElement rem1;
-//	
-//	@FindBy(xpath="//div[contains(text(),' results')]") //xpath for displaying the total count that displayed after removing
-//	public WebElement countResult;
-//	
-//}
-=======
-<<<<<<< HEAD
-	@FindBy(xpath="//span[text()='Logout']")
-	public WebElement logout; //xpath for logout
-	
-=======
-<<<<<<< HEAD
->>>>>>> ee91d922dd66522ba8552e58e39863a384d96d5c
-
 }
 	
 
-=======
->>>>>>> e1b89dfce1145d8aaf16c43e166302f9b6bcfbe1
->>>>>>> 3001778f8f7b7cb2f4d30d5867dec82be0987e19
-
-}
