85c55071437b8b4be2aebfa8682ba14f37702879
Lavanya1773 <74286126+Lavanya1773@users.noreply.github.com>
2023-11-27 12:20:45 +0530

    runnerVoucher.java

--- src/test/java/com/runner/runnerVoucher.java
+++ src/test/java/com/runner/runnerVoucher.java
@@ -1,14 +1,1 @@
-//package com.runner;
-//import org.junit.runner.RunWith;
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/resources/Features/message.feature", glue = "com.stepdefinitions",plugin = {"pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-////@CucumberOptions(features = "src/test/resources/Features", glue = "com.stepdefinitions",plugin = {"pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-////@CucumberOptions(features = "src/test/resources/Features/voucher.feature", glue = "com.stepdefinitions",plugin = {"pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
-//
-//public class runnerVoucher {
-//
-//	
-//}
+
