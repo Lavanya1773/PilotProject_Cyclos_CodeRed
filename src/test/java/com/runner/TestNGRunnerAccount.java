246a64484f6716453e52094087b5915b379dcbd7
Lavanya1773 <74286126+Lavanya1773@users.noreply.github.com>
2023-11-27 12:19:13 +0530

    TestNGRunnerAccount.java

--- src/test/java/com/runner/TestNGRunnerAccount.java
+++ src/test/java/com/runner/TestNGRunnerAccount.java
@@ -1,14 +1,1 @@
-//package com.runner;
-//import org.testng.annotations.DataProvider;
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-//
-//@CucumberOptions(features = "src/test/resources/Features", glue = "com.stepdefinitions")
-//public class TestNGRunnerAccount extends AbstractTestNGCucumberTests{
-//	@Override
-//	@DataProvider(parallel = true)
-//	public Object[][] scenarios(){
-//		return super.scenarios();
-//	}
-//
-//}
+
