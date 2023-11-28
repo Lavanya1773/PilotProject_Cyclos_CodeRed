1ed114f79c62dc3a581c135ecd4b815c2066a13a
Lavanya1773 <lavanyasethuraj1703@gmail.com>
2023-11-28 10:30:11 +0530

    runner files updated by lavanya

--- src/test/java/com/runner/TestCodeRed_RunnerClass.java
+++ src/test/java/com/runner/TestCodeRed_RunnerClass.java
@@ -3,7 +3,7 @@ package com.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features="src/test/resources/Features",
-glue="com.stepdefinitions",plugin = "json:target/cucumber-report.json")
+glue="com.stepdefinitions",plugin = {"json:target/cucumber-report.json","pretty", "html:target/Reports/Cucuber.html", "junit:target/Reports/Cucuber.xml", "json:target/Reports/Cucuber.json"})
 public class TestCodeRed_RunnerClass extends AbstractTestNGCucumberTests{
  
 }
